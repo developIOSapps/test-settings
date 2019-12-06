c42a0befc48a1ad47d2c6eac596881ac9f154613
Steven Hertz <hertzsteven@icloud.com>
2019-12-06 00:16:53 -0500

    Initial Commit

---
+++ test settings/AppDelegate.swift
@@ -0,0 +1,57 @@
+//
+//  AppDelegate.swift
+//  test settings
+//
+//  Created by Steven Hertz on 12/5/19.
+//  Copyright © 2019 DIA. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        registerDefaultsFromSettingsBundle()
+        return true
+    }
+    
+    func registerDefaultsFromSettingsBundle()
+    {
+        let settingsUrl = Bundle.main.url(forResource: "Settings", withExtension: "bundle")!.appendingPathComponent("Root.plist")
+        let settingsPlist = NSDictionary(contentsOf:settingsUrl)!
+        let preferences = settingsPlist["PreferenceSpecifiers"] as! [NSDictionary]
+
+        var defaultsToRegister = Dictionary<String, Any>()
+
+        for preference in preferences {
+            guard let key = preference["Key"] as? String else {
+                NSLog("Key not fount")
+                continue
+            }
+            print(preference["DefaultValue"])
+            defaultsToRegister[key] = preference["DefaultValue"]
+        }
+        UserDefaults.standard.register(defaults: defaultsToRegister)
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
---
+++ test settings/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  test settings
+//
+//  Created by Steven Hertz on 12/5/19.
+//  Copyright © 2019 DIA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(UserDefaults.standard.object(forKey: "name_preference") as Any)
+        //UserDefaults.standard.set(1, forKey: "teacher")
+        print(UserDefaults.standard.object(forKey: "teacher") as Any)
+        // UserDefaults.standard.set("NOOOO", forKey: "name_preference")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
